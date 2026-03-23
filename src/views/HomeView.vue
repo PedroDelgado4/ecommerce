4ca5145d8820dda78212d917fc0059f0cdf4f0e9
Pedro Delgado Durán <fpedrodelgadoduran@gmail.com>
2026-03-23 12:56:31 +0100

    Render dynamic product list using v-for and tailwind grid

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,15 +3,42 @@ import { useCartStore } from '../stores/cart';
 
 const cart = useCartStore();
 
+// Simulacion de DB de cursos
+const products = [
+    { id: 1, name: 'Curso Vue', price: 49.99, image: 'https://placehold.co/300x200?text=Vue+3' },
+    { id: 2, name: 'Pinia Masterclass', price: 29.99, image: 'https://placehold.co/300x200?text=Pinia'},
+    { id: 3, name: 'Tailwind para Pros', price: 34.99, image: 'https://placehold.co/300x200?text=Tailwind' },
+    { id: 4, name: 'Typescript Avanzado', price: 54.99, image: 'https://placehold.co/300x200?text=TypeScript' }
+]
+
 </script>
 
 <template>
     <div class="p-8">
-        <h1 class="text-3xl font-bold text-blue-600">Tienda de cursos online</h1>
-        <p class="mt-4">Productos del carrito: {{cart.totalItems}}</p>
-        <button 
-        @click="cart.addToCart({id: 1, name: 'Curso Vue', price: 50})" 
-        class="mt-4 px-4 py-2 bg-black text-white rounded hover:bg-gray-800 transition-colors">Comprar</button>
-    </div>
+        <header class="flex justify-between items-center mb-8">
+            <h1 class="text-3xl font-bold text-gray-800">Cursos disponibles</h1>
+            <div class="bg-blue-100 text-blue-800 px-4 py-2 rounded-full font-semibold">
+                Productos en el carrito: {{ cart.totalItems }} | Precio total: {{ cart.totalPrice }}
+            </div>
+        </header>
+        
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+            <div
+                v-for="product in products"
+                :key="product.id"
+                class="border rounded-lg overflow-hidden shadow-sm hover:shadow-md transition-shadow">
+                <img :src="product.image" alt="Curso {{ product.name }}" class="w-full h-48 object-cover"/>
 
+                <div class="p-4">
+                    <h2 class="text-xl font-semibold">{{ product.name }}</h2>
+                    <p class="text-gray-600 mt-2">{{ product.price }}</p>
+                    <button 
+                        @click="cart.addToCart(product)"
+                        class="mt-4 w-full bg-blue-600 text-white py-2 rounded-md hover:bg-blue transition-colors">
+                        Añadir al carrito
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
